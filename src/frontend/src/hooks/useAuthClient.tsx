ab94af34e76521eada913e78c7f5f7a08a6db889
Dominik <bendominik@gmail.com>
2024-11-23 23:10:52 +0100

    feat: update backend agent

--- src/frontend/src/hooks/useAuthClient.tsx
+++ src/frontend/src/hooks/useAuthClient.tsx
@@ -3,20 +3,31 @@
 
 import { AuthClient } from "@dfinity/auth-client";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import { Actor, ActorSubclass, HttpAgent, Identity } from "@dfinity/agent";
 import {
-  canisterId,
-  createActor,
-  CreateActorOptions
-} from "declarations/users-rs";
+  canisterId as usersInterRsCanisterId,
+  idlFactory as usersInterRsIdl
+} from "declarations/users-inter-rs";
+import { _SERVICE as UsersInterRsService } from "declarations/users-inter-rs/users-inter-rs.did";
+import { Principal } from "@dfinity/principal";
 
-const AuthContext = createContext<any>(null);
+type AuthContextType = {
+  isAuthenticated: boolean;
+  login: (func?: () => void) => void;
+  logout: () => void;
+  authClient: AuthClient | null;
+  identity: Identity | null;
+  principal: Principal | null;
+  usersInterRsActor: ActorSubclass<UsersInterRsService>;
+  loading: boolean;
+};
+
+const AuthContext = createContext<AuthContextType | null>(null);
 
 export const getIdentityProvider = () => {
   let idpProvider;
-  // Safeguard against server rendering
   if (typeof window !== "undefined") {
     const isLocal = process.env.DFX_NETWORK !== "ic";
-    // Safari does not support localhost subdomains
     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
     if (isLocal && isSafari) {
       idpProvider = `http://localhost:4943/?canisterId=${process.env.CANISTER_ID_INTERNET_IDENTITY}`;
@@ -40,20 +51,12 @@ export const getDerivationOrigin = () => {
   return derivationOrigin;
 };
 
-
 export const defaultOptions = {
-  /**
-   *  @type {import("@dfinity/auth-client").AuthClientCreateOptions}
-   */
   createOptions: {
     idleOptions: {
-      // Set to true if you do not want idle functionality
       disableIdle: true
     }
   },
-  /**
-   * @type {import("@dfinity/auth-client").AuthClientLoginOptions}
-   */
   loginOptions: {
     identityProvider: getIdentityProvider(),
     windowOpenerFeatures:
@@ -62,43 +65,23 @@ export const defaultOptions = {
   }
 };
 
-/**
- *
- * @param options - Options for the AuthClient
- * @param {AuthClientCreateOptions} options.createOptions - Options for the AuthClient.create() method
- * @param {AuthClientLoginOptions} options.loginOptions - Options for the AuthClient.login() method
- * @returns
- */
 export const useAuthClient = (options = defaultOptions) => {
-  console.log("identityProviderOptions", getIdentityProvider());
-
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [authClient, setAuthClient] = useState(null);
+  const [authClient, setAuthClient] = useState<AuthClient | null>(null);
   const [identity, setIdentity] = useState(null);
   const [principal, setPrincipal] = useState(null);
-  const [whoamiActor, setWhoamiActor] = useState(null);
+  const [usersInterRsActor, setUsersInterRsActor] =
+    useState<UsersInterRsService | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    // Initialize AuthClient
     AuthClient.create(options.createOptions).then(async (client) => {
-      updateClient(client);
+      await updateClient(client);
+      setLoading(false);
     });
   }, []);
 
-  const login = () => {
-    authClient.login({
-      ...options.loginOptions,
-      onSuccess: () => {
-        console.log("success!!!!!!", authClient);
-        updateClient(authClient);
-      },
-      onError: () => {
-        console.log("meh... ;/", authClient);
-      }
-    });
-  };
-
-  async function updateClient(client) {
+  const updateClient = async (client: AuthClient) => {
     const isAuthenticated = await client.isAuthenticated();
     setIsAuthenticated(isAuthenticated);
 
@@ -110,38 +93,58 @@ export const useAuthClient = (options = defaultOptions) => {
 
     setAuthClient(client);
 
-    const actor = createActor(canisterId, {
-      agentOptions: {
-        identity
-      }
-    } as CreateActorOptions);
+    const agent = new HttpAgent({ identity });
+    if (process.env.DFX_NETWORK !== "ic") {
+      agent.fetchRootKey();
+    }
 
-    setWhoamiActor(actor);
-  }
+    const actor = Actor.createActor<UsersInterRsService>(usersInterRsIdl, {
+      agent,
+      canisterId: usersInterRsCanisterId
+    });
+    setUsersInterRsActor(actor);
+  };
 
-  async function logout() {
-    await authClient?.logout();
-    await updateClient(authClient);
-  }
+  const logout = async () => {
+    if (authClient) {
+      await authClient.logout();
+      setIsAuthenticated(false);
+      setIdentity(null);
+      setPrincipal(null);
+    }
+  };
 
   return {
     isAuthenticated,
-    login,
+    login: async (callback?: () => void) => {
+      await authClient?.login({
+        ...options.loginOptions,
+        onSuccess: () => {
+          updateClient(authClient!);
+          if (callback) callback();
+        },
+        onError: () => console.error("Couldn't authorize user.")
+      });
+    },
     logout,
     authClient,
+    loading,
     identity,
     principal,
-    whoamiActor
+    usersInterRsActor
   };
 };
 
-/**
- * @type {React.FC}
- */
-export const AuthProvider = ({ children }) => {
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const auth = useAuthClient();
 
   return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 };
 
-export const useAuth = () => useContext(AuthContext);
+export const useApi = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
